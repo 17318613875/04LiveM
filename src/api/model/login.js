f4abbc78e4b9e5b97b404a6b3c546bd14b7ee96a
彭斌斌 <binbin@mgtv.com>
2020-04-21 20:21:24 +0800

    feat(login.js): aPI 添加loginAPI

--- src/api/model/login.js
+++ src/api/model/login.js
@@ -1,16 +1,10 @@
-/*
- * @Description: login
- * @Version: 1.0
- * @Autor: wugeng
- * @Date: 2019-10-25 09:45:55
- * @LastEditors: wugeng
- * @LastEditTime: 2019-10-25 14:38:39
- */
-import axios from '@/plugin/axios'
-
+import axios from '@/plugin/axios';
 let login = {
-  AccountLogin (data) {
-    return axios.post('/login', data)
-  }
-}
-export default login
+  AccountLogin(data) {
+    return axios.post('/media/auth/user/login', {
+      appId: process.env.VUE_APP_USER_APPID,
+      data,
+    });
+  },
+};
+export default login;
