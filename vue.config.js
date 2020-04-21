60de431ab4ca27bd23507f3e7e0bbc0d53321ad3
彭斌斌 <binbin@mgtv.com>
2020-04-21 20:18:02 +0800

    docs(vue.config.js): 配置 API proxy

--- vue.config.js
+++ vue.config.js
@@ -6,18 +6,18 @@
  * @LastEditors: wugeng
  * @LastEditTime: 2019-10-25 15:25:40
  */
-const VueFilenameInjector = require('./tools/vue-filename-injector')
-const CompressionPlugin = require('compression-webpack-plugin')
+const VueFilenameInjector = require('./tools/vue-filename-injector');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 // 拼接路径
-const resolve = dir => require('path').join(__dirname, dir)
+const resolve = (dir) => require('path').join(__dirname, dir);
 
 // 增加环境变量
-process.env.VUE_APP_VERSION = require('./package.json').version
-process.env.VUE_APP_BUILD_TIME = require('dayjs')().format('YYYY-M-D HH:mm:ss')
+process.env.VUE_APP_VERSION = require('./package.json').version;
+process.env.VUE_APP_BUILD_TIME = require('dayjs')().format('YYYY-M-D HH:mm:ss');
 
 // 基础路径 注意发布之前要先修改这里
-let publicPath = '/'
+let publicPath = '/';
 
 module.exports = {
   publicPath, // 根据你的实际情况更改这里
@@ -27,46 +27,52 @@ module.exports = {
     disableHostCheck: true,
     publicPath: publicPath, // 和 publicPath 保持一致,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        secure: false
-      }
-    }
+      '/api/': {
+        target: 'http://10.200.12.153:8082',
+        secure: false,
+        pathRewrite: {
+          '^/api/': '/',
+        },
+      },
+      '/media/': {
+        target: 'http://10.200.12.121:8005',
+        changeOrigin: true,
+      },
+    },
   },
   css: {
     loaderOptions: {
       // 设置 scss 公用变量文件
       sass: {
-        data: `@import '~@/assets/style/public.scss';`
-      }
-    }
+        data: `@import '~@/assets/style/public.scss';`,
+      },
+    },
   },
   // 默认设置: https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-service/lib/config/base.js
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     /**
-         * 删除懒加载模块的 prefetch preload，降低带宽压力
-         * https://cli.vuejs.org/zh/guide/html-and-static-assets.html#prefetch
-         * https://cli.vuejs.org/zh/guide/html-and-static-assets.html#preload
-         * 而且预渲染时生成的 prefetch 标签是 modern 版本的，低版本浏览器是不需要的
-         */
-    config.plugins
-      .delete('prefetch')
-      .delete('preload')
+     * 删除懒加载模块的 prefetch preload，降低带宽压力
+     * https://cli.vuejs.org/zh/guide/html-and-static-assets.html#prefetch
+     * https://cli.vuejs.org/zh/guide/html-and-static-assets.html#preload
+     * 而且预渲染时生成的 prefetch 标签是 modern 版本的，低版本浏览器是不需要的
+     */
+    config.plugins.delete('prefetch').delete('preload');
     // 解决 cli3 热更新失效 https://github.com/vuejs/vue-cli/issues/1559
-    config.resolve
-      .symlinks(true)
+    config.resolve.symlinks(true);
     config
-    // 开发环境
-      .when(process.env.NODE_ENV === 'development',
+      // 开发环境
+      .when(
+        process.env.NODE_ENV === 'development',
         // sourcemap不包含列信息
-        config => config.devtool('cheap-source-map')
+        (config) => config.devtool('cheap-source-map')
       )
-    // TRAVIS 构建 vue-loader 添加 filename
-      .when(process.env.VUE_APP_BUILD_MODE === 'TRAVIS' || process.env.NODE_ENV === 'development',
+      // TRAVIS 构建 vue-loader 添加 filename
+      .when(
+        process.env.VUE_APP_BUILD_MODE === 'TRAVIS' || process.env.NODE_ENV === 'development',
         VueFilenameInjector(config, {
-          propName: process.env.VUE_APP_SOURCE_VIEWER_PROP_NAME
+          propName: process.env.VUE_APP_SOURCE_VIEWER_PROP_NAME,
         })
-      )
+      );
     // i18n
     /* config.module
                                       .rule('i18n')
@@ -75,28 +81,25 @@ module.exports = {
                                       .loader('@kazupon/vue-i18n-loader')
                                       .end() */
     // svg
-    const svgRule = config.module.rule('svg')
-    svgRule.uses.clear()
-    svgRule
-      .include
+    const svgRule = config.module.rule('svg');
+    svgRule.uses.clear();
+    svgRule.include
       .add(resolve('src/assets/svg-icons/icons'))
       .end()
       .use('svg-sprite-loader')
       .loader('svg-sprite-loader')
       .options({
-        symbolId: 'd2-[name]'
+        symbolId: 'd2-[name]',
       })
-      .end()
+      .end();
     // image exclude
-    const imagesRule = config.module.rule('images')
+    const imagesRule = config.module.rule('images');
     imagesRule
       .test(/\.(png|jpe?g|gif|webp|svg)(\?.*)?$/)
-      .exclude
-      .add(resolve('src/assets/svg-icons/icons'))
-      .end()
+      .exclude.add(resolve('src/assets/svg-icons/icons'))
+      .end();
     // 重新设置 alias
-    config.resolve.alias
-      .set('@api', resolve('src/api'))
+    config.resolve.alias.set('@api', resolve('src/api'));
     // 判断环境加入模拟数据
     /* const entry = config.entry('app')
                                      if (process.env.VUE_APP_BUILD_MODE !== 'nomock') {
@@ -107,15 +110,15 @@ module.exports = {
   },
   configureWebpack: (config) => {
     if (process.env.NODE_ENV !== 'development') {
-      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true;
       config.plugins = [
         ...config.plugins,
         new CompressionPlugin({
           test: /\.js$|\.html$|.\css/, // 匹配文件名
           threshold: 10240, // 对超过10k的数据压缩
-          deleteOriginalAssets: false // 不删除源文件
-        })
-      ]
+          deleteOriginalAssets: false, // 不删除源文件
+        }),
+      ];
     }
-  }
-}
+  },
+};
