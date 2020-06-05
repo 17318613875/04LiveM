cab7fecf071596bd3cb714c88c694927aef3f5bf
彭斌斌 <binbin@mgtv.com>
2020-06-05 17:56:48 +0800

    docs(basedata.js): 添加 基础column

---
+++ src/config/baseData.js
@@ -0,0 +1,8 @@
+export const BaseFormType = [
+  { key: "creator" },
+  { key: "updater" },
+  { key: "creatorName", searchType: "input" },
+  { key: "updaterName", searchType: "input" },
+  { key: "createTime" },
+  { key: "updateTime" },
+];
